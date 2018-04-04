212842771ed64be3be461d0cd8b78da65b2c1a15
Nate McMaster <nate.mcmaster@microsoft.com>
2018-04-04 15:29:10 -0700

    Add sample to demonstrate site extension usage [must be removed before merging to master]

---
+++ samples/DogeApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace DogeApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseSetting(WebHostDefaults.HostingStartupExtensionsKey, @"..\DogeExtension\bin\Debug\netstandard2.0\startupextension.config")
+                .UseStartup<Startup>();
+    }
+}
---
+++ samples/DogeExtension/DogeStartup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(SampleStartupExtension.DogeStartup))]
+
+namespace SampleStartupExtension
+{
+    // redirects all requests to a picture of doge
+    public class DogeStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            // such startup!
+            builder.ConfigureServices(services =>
+                            services.AddSingleton<IStartupFilter, DogeStartupFilter>());
+        }
+
+        private class DogeStartupFilter : IStartupFilter
+        {
+            // much configure
+            public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+            {
+                return app =>
+                {
+                    app.Use(async (ctx, n) =>
+                    {
+                        // Wow rewrite path!
+                        ctx.Request.Path = "/doge.jpg";
+                        await n();
+                    });
+
+                    next(app);
+                };
+            }
+        }
+    }
+}
